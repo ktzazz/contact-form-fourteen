095acccdf4620694008f205c899a3850c2e2ca06
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-05-18 15:01:42 -0600

    starting the js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const radioInputs = document.querySelectorAll('.radio-input');
-
-  radioInputs.forEach(input => {
-    input.addEventListener('change', function() {
-      // the event change is fired when the input is checked or unchecked
-      radioInputs.forEach(otherInput => {
-        otherInput.closest('.radio-option').classList.remove('selected');
-      }); //the method closest() returns the closest ancestor element that matches the specified selector ".radio-option" 
-          // and removes the "selected" class from all radio options that are not currently selected
-      if (this.checked) { //adds the "selected" class to the currently selected radio option and now the css can be applied
-        this.closest('.radio-option').classList.add('selected');
-      }
-    });
-  });
-});
 
 // makes clickeable the div with the class "radio-option" so it can mark the radio button as checked, even if the user doesn't click on the radio button 
 const radioOptions = document.querySelectorAll('.radio-option');
@@ -27,3 +12,22 @@ const radioOptions = document.querySelectorAll('.radio-option');
       }
     });
   });
+
+// adds the style to the box when the radio button is checked
+const radioInputs = document.querySelectorAll('.radio-input'); 
+
+ radioInputs.forEach(input => {
+    input.addEventListener('change', function() {
+      // the event change is fired when the input is checked or unchecked
+      radioInputs.forEach(otherInput => {
+        otherInput.closest('.radio-option').classList.remove('selected');
+      }); //the method closest() returns the closest ancestor element that matches the specified selector ".radio-option" 
+          // and removes the "selected" class from all radio options that are not currently selected
+      if (this.checked) { //adds the "selected" class to the currently selected radio option and now the css can be applied
+        this.closest('.radio-option').classList.add('selected');
+      }
+    });
+  });
+});
+
+ const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
